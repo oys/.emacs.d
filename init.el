db20ca9328c34ff3b3de0090f04630201d2506fe
oys <lynnx1029@gmail.com>
2016-03-27 19:03:37 +0900

    Apply solarized-dark color theme.

--- init.el
+++ init.el
@@ -165,7 +165,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default-input-method "korean-hangul"))
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+ '(default-input-method "korean-hangul")
+ '(frame-background-mode (quote dark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -175,3 +179,7 @@
 
 ;; Show sr-sppedbar window on the left.
 (setq sr-speedbar-right-side nil)
+
+;; Load the solarized-dark color theme.
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
+(load-theme 'solarized t)
