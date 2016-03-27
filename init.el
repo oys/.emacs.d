6268a94db9e7dbff5845ca56bd1b3859e87ee0b0
oys <lynnx1029@gmail.com>
2016-03-27 18:28:00 +0900

    Add input method and change default font.

--- init.el
+++ init.el
@@ -159,3 +159,15 @@
 
 ;; Package zygospore
 (global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default-input-method "korean-hangul"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Iosevka" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
