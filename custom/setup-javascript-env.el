b353096c77e7135c098b35c9c247ac234cea2e54
oys <lynnx1029@gmail.com>
2016-11-23 13:05:57 +0900

    Turn on yasnippet minor mode when js-mode enabled

--- custom/setup-javascript-env.el
+++ custom/setup-javascript-env.el
@@ -1,6 +1,8 @@
 (require 'tern)
 
 (add-to-list 'company-backends 'company-tern)
-(add-hook 'js-mode-hook (lambda() (tern-mode t)))
+(add-hook 'js-mode-hook (lambda()
+                          (tern-mode t)
+                          (yas-minor-mode t)))
 
 (provide 'setup-javascript-env)
